cb4e32523edc09901a2312259b598ed62cbfe00b
anna-vasylenko <anna.bilych16@gmail.com>
2024-08-27 00:33:01 +0300

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,23 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import s from "./HomePage.module.css";
+import { selectIsLoggedIn } from "../../redux/auth/selectors";
 
 const HomePage = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
   return (
     <>
       <div className={s.homePage}>
         <h1 className={s.title}>Welcome to Your Personal Phonebook!</h1>
-        <Link className={s.button} to="/login">
-          Get started
-        </Link>
+        {isLoggedIn ? (
+          <Link className={s.button} to="/contacts">
+            Phonebook
+          </Link>
+        ) : (
+          <Link className={s.button} to="/login">
+            Get started
+          </Link>
+        )}
       </div>
     </>
   );
