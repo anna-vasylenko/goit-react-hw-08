798b77c070b4724f2e9991fc5b2f24f65866f708
anna-vasylenko <anna.bilych16@gmail.com>
2024-08-27 15:49:11 +0300

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { lazy, Suspense, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Loader from "./components/Loader/Loader";
 import { selectIsError, selectIsRefreshing } from "./redux/auth/selectors";
@@ -15,7 +15,6 @@ const RegistrationPage = lazy(() =>
 );
 const LoginPage = lazy(() => import("./pages/LoginPage/LoginPage"));
 const ContactsPage = lazy(() => import("./pages/ContactsPage/ContactsPage"));
-const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage"));
 
 function App() {
   const dispatch = useDispatch();
@@ -64,7 +63,7 @@ function App() {
             }
           />
         </Route>
-        <Route path="*" element={<NotFoundPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Suspense>
   );
