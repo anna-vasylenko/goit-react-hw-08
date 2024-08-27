e7b6a455acd0d76bc3d7a723c96972bf2f2f4b98
anna-vasylenko <anna.bilych16@gmail.com>
2024-08-27 15:06:42 +0300

    Update RestrictedRoute.jsx

--- src/components/RestrictedRoute.jsx
+++ src/components/RestrictedRoute.jsx
@@ -5,7 +5,7 @@ import { Navigate } from "react-router-dom";
 const RestrictedRoute = ({ children }) => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
 
-  return isLoggedIn ? <Navigate to="./contacts" /> : children;
+  return isLoggedIn ? <Navigate to="/contacts" /> : children;
 };
 
 export default RestrictedRoute;
