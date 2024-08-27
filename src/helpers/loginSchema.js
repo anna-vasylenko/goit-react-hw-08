ddabaed307f34f8a7217ab621a94b1b93f2c38be
anna-vasylenko <anna.bilych16@gmail.com>
2024-08-27 13:31:37 +0300

    update

--- src/helpers/loginSchema.js
+++ src/helpers/loginSchema.js
@@ -1,7 +1,7 @@
 import * as Yup from "yup";
 
 export const loginSchema = Yup.object().shape({
-  email: Yup.string().trim().email().required("Email is required"),
+  email: Yup.string().email("Must be a valid email").required("Required"),
   password: Yup.string()
     .min(3, "Too Short!")
     .max(50, "Too Long!")
--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-// axios.defaults.baseURL = "https://connections-api.goit.global";
-
 export const fetchContacts = createAsyncThunk(
   "contacts/fetchAll",
   async (_, thunkAPI) => {
