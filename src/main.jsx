ddfe28fa6329ea4c4db2a02b35181dd3de8042ae
anna-vasylenko <anna.bilych16@gmail.com>
2024-09-19 21:36:17 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
-import App from "./App.jsx";
-import "./index.css";
-import "modern-normalize";
 import { persistor, store } from "./redux/store.js";
 import { Toaster } from "react-hot-toast";
+import "modern-normalize";
+
+import App from "./App.jsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
